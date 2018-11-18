8e561e126b95f284a6901bc15ea33a8bffa9c6ed
Luke Kozak <kozakluke@gmail.com>
2018-11-18 21:33:38 +0100

    Create dashoffset-sample

---
+++ src/pages/dashoffset-sample/DashoffsetSample.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import * as style from './DashoffsetSample.pcss';
+
+export default function DashoffsetSample()
+{
+    return (
+        <div className={style.content}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500">
+                <polyline
+                    className={style.line}
+                    points="80,450 200,100 300,310 380,140 420,400"/>
+            </svg>
+        </div>
+    );
+}
