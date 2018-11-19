471bbce7c35464d9c12b3c73451cf8286ad8abd6
Luke Kozak <kozakluke@gmail.com>
2018-11-19 22:53:38 +0100

    Clean pattern-sample

--- src/pages/pattern-sample/PatternSample.pcss.d.ts
+++ src/pages/pattern-sample/PatternSample.pcss.d.ts
@@ -1,5 +1,1 @@
-export const areas:string;
-export const header:string;
-export const menu:string;
-export const content:string;
-export const footer:string;
+export const pattern:string;
--- src/pages/pattern-sample/PatternSample.tsx
+++ src/pages/pattern-sample/PatternSample.tsx
@@ -4,16 +4,13 @@ import * as style from './PatternSample.pcss';
 export default function PatternSample()
 {
     return (
-        <div className={style.content}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500">
-                <defs>
-                    <pattern id="diamond" width="50" height="50" patternUnits="userSpaceOnUse">
-                        <rect x="15" y="15" width="20" height="20" transform="rotate(45 25,25)"/>
-                    </pattern>
-                </defs>
-                
-                <rect fill="url(#diamond)" width="500" height="500"/>
-            </svg>
-        </div>
+        <svg className={style.pattern} viewBox="0 0 500 500">
+            <defs>
+                <pattern id="diamond" width="50" height="50" patternUnits="userSpaceOnUse">
+                    <rect x="15" y="15" width="20" height="20" transform="rotate(45 25,25)"/>
+                </pattern>
+            </defs>
+            <rect fill="url(#diamond)" width="500" height="500"/>
+        </svg>
     );
 }
